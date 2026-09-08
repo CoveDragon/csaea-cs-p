f90427bb9b74d86fc4e1358a3e25c5c86fb55bb2
Zack <zackary.beitz@gmail.com>
2026-09-08 18:22:24 +0000

    day-01-commit

---
+++ crash-course.py
@@ -0,0 +1,35 @@
+# comment
+
+# here
+# is
+# a
+# comment
+
+print("hello, world!")
+
+# VARIABLE DECLARATIONS AND DATA TYPES
+
+a = 4 # integer
+b = 5.5 # float
+c = "CSAEA" # string
+d = False # boolean
+
+print(a,b,c,d)
+
+# OPERATORS
+# + - / * % ** //
+# += -= /=
+
+e = 3 ** 3
+print(e)
+e += 7
+print(e)
+
+# f-string
+
+print(f"e is equal to {e}")
+
+e -= 6.5
+e += 5.1
+
+print(f"wait nevermind e is equal to {e}")
